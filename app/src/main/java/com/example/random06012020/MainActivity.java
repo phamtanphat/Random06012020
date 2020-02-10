53aa23506d7539e5c6d6502418657c51350f32a3
phatdroid94@gmail.com <phatdroid94@gmail.com>
2020-02-10 20:13:43 +0700

    ui app loto

--- app/src/main/java/com/example/random06012020/MainActivity.java
+++ app/src/main/java/com/example/random06012020/MainActivity.java
@@ -14,37 +14,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Cách 1 : Dùng lớp random
-//        0 -> 10
-        // 75 => 133 => tìm công thức
-        Random random = new Random();
-        for (int i = 0 ; i < 1000 ; i++){
-//            (max - min + 1) + min
-            int value = random.nextInt(133 - 75 + 1) + 75;
-            Log.d("BBB",value + "");
-        }
-
-
-        // Cách 2 : Math
-        // Hàm làm tròn : Math.round
-        // Hàm làm tròn lên : Math.ceil()
-        // Hàm làm tròn xuống : Math.floor()
-//
-//        5 ~ 10
-//        for (int i = 0 ; i < 100 ; i++){
-//            double numberRandom = (Math.random() * 5) + 5;
-//            long value = Math.round(numberRandom);
-//            Log.d("BBB", String.valueOf(value));
-//        }
-
-//        double value = Math.floor(numberRandom);
-//        Log.d("BBB",String.valueOf(value));
-
-        // 1 : Phím tắt hỗ trợ fix lỗi : alt + enter
-        // 2 : Xem cách fix trên stackoverflow
-        // 3 :  Coi document của object
-
-        //builtin - function
-//        Log.d("BBB","abc");
     }
 }
