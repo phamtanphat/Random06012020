a515f8a9423a2d7838b15742e4333c1f84ed1699
phatdroid94@gmail.com <phatdroid94@gmail.com>
2020-02-10 20:29:07 +0700

    list task

--- app/src/main/java/com/example/random06012020/MainActivity.java
+++ app/src/main/java/com/example/random06012020/MainActivity.java
@@ -4,15 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText mEdtSomin,mEdtSomax;
+    Button mBtnRandom;
+    TextView mTvKetqua;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mEdtSomax = findViewById(R.id.edtSomax);
+        mEdtSomin = findViewById(R.id.edtSomin);
+        mBtnRandom = findViewById(R.id.btnRandom);
+        mTvKetqua = findViewById(R.id.tvKetqua);
+
+        // Task 1 : Kiểm tra có dữ liệu trong 2 Edittext hay không
+        // Task 2 : Nếu số max < số min , số max = smin + 1;
+        // Task 3 : Click button random trả về số trong khoản min và max
+        // Task 4 : Nối chuỗi dữ liệu các giá trị random (5 - 3 - 2 - 1 -)
 
     }
 }
